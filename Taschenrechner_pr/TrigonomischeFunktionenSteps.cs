a5cd044f8252837e8724b32e817cf7c23643b066
Gettinger Nicholas <wi18b038@technikum-wien.at>
2020-11-05 10:24:17 +0100

    Projektdateien hinzufügen.

---
+++ Taschenrechner_pr/TrigonomischeFunktionenSteps.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace Taschenrechner_pr
+{
+    [Binding]
+    public class TrigonomischeFunktionenSteps
+    {
+        private double number;
+
+        [Given(@"the first number is (.*)")]
+        public void GivenTheFirstNumberIs(Decimal p0)
+        {
+            number = (double) p0;
+        }
+        
+        [When(@"we calculate the sinus from it")]
+        public void WhenWeCalculateTheSinusFromIt()
+        {
+            number = Math.Sin(number);
+        }
+        
+        [When(@"we calculate the cosinus from it")]
+        public void WhenWeCalculateTheCosinusFromIt()
+        {
+            number = Math.Cos(number);
+        }
+        
+        [When(@"we calculate the tangens from it")]
+        public void WhenWeCalculateTheTangensFromIt()
+        {
+            number = Math.Tan(number);
+        }
+        
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int p0)
+        {
+            var expected = p0;
+            var actual = number;
+            Assert.Equal(expected, actual, 5);
+        }
+    }
+}
---
+++ Taschenrechner_pr/taschenrechner.feature
@@ -0,0 +1,22 @@
+﻿Feature: Trigonomische Funktionen
+Als Taschenrechner Benützer
+möchte ich trigonomische funktionen Berechnen können
+damit ich ein Dreieck berechnen lassen kann
+
+@mytag
+Scenario: sinus
+	Given the first number is 3.14159
+	When we calculate the sinus from it
+	Then the result should be 0
+
+	@mytag
+Scenario: cosinus
+	Given the first number is 3.14159
+	When we calculate the cosinus from it
+	Then the result should be -1
+
+	@mytag
+Scenario: tangens
+	Given the first number is 3.14159
+	When we calculate the tangens from it
+	Then the result should be 0
